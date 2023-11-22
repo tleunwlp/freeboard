2f74bab3016183cbe2e79b814eb73955be049399
sieun0 <evensomere@gmail.com>
2023-11-22 21:34:16 +0900

    state 변수 추가

--- pages/boards/new/index.js
+++ pages/boards/new/index.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { useState } from "react";
 import {
   Wrapper,
   Title,
@@ -26,26 +27,101 @@ import {
 export default function ResisterPage() {
   // 자바스크립트 쓰는 곳
 
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const [nameError, setNameError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [titleError, setTitleError] = useState("");
+  const [contentError, setContentError] = useState("");
+
+  function onChangeName(event) {
+    setName(event.target.value);
+    if (event.target.value !== "") {
+      setNameError("");
+    }
+  }
+
+  function onChangePassword(event) {
+    setPassword(event.target.value);
+    if (event.target.value !== "") {
+      setPasswordError("");
+    }
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+    if (event.target.value !== "") {
+      setTitleError("");
+    }
+  }
+
+  function onChangeContent(event) {
+    setContent(event.target.value);
+    if (event.target.value !== "") {
+      setContentError("");
+    }
+  }
+
+  function onClickResister() {
+    if (name === "") {
+      setNameError("이름을 작성해주세요.");
+    }
+    if (password === "") {
+      setPasswordError("비밀번호를 입력해주세요.");
+    }
+    if (title === "") {
+      setTitleError("제목을 입력해주세요.");
+    }
+    if (content === "") {
+      setContentError("내용을 입력해주세요.");
+    }
+    if (name && password && title && content) {
+      alert("등록되었습니다.");
+    }
+  }
+
   return (
     <Wrapper>
       <Title>게시물 등록</Title>
       <Wrapper_info>
         <InputWrapper>
           <Label>작성자</Label>
-          <Writer type="text" placeholder="이름을 적어주세요." />
+          <Writer
+            type="text"
+            placeholder="이름을 적어주세요."
+            onChange={onChangeName}
+          />
+          <div>{nameError}</div>
         </InputWrapper>
         <InputWrapper>
           <Label>비밀번호</Label>
-          <Password type="text" placeholder="비밀번호를 입력해주세요." />
+          <Password
+            type="text"
+            placeholder="비밀번호를 입력해주세요."
+            onChange={onChangePassword}
+          />
+          <div>{passwordError}</div>
         </InputWrapper>
       </Wrapper_info>
       <InputWrapper>
         <Label>제목</Label>
-        <Subject type="text" placeholder="제목을 작성해주세요." />
+        <Subject
+          type="text"
+          placeholder="제목을 작성해주세요."
+          onChange={onChangeTitle}
+        />
+        <div>{titleError}</div>
       </InputWrapper>
       <InputWrapper>
         <Label>내용</Label>
-        <Contents placeholder="내용을 작성해주세요." />
+        <Contents
+          placeholder="내용을 작성해주세요."
+          onChange={onChangeContent}
+        />
+        <div>{contentError}</div>
       </InputWrapper>
       <InputWrapper>
         <Label>주소</Label>
@@ -74,7 +150,7 @@ export default function ResisterPage() {
         <RadioLabel htmlFor="image">사진</RadioLabel>
       </MainSetWrapper>
       <ButtonWrapper>
-        <RegButton>등록하기</RegButton>
+        <RegButton onClick={onClickResister}>등록하기</RegButton>
       </ButtonWrapper>
     </Wrapper>
   );
